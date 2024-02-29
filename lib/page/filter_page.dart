b5be4c6c494d2bae117367d149a1cdc190887b8d
WilliamSilvaGh <williamsilvaesquilino@gmail.com>
2024-02-28 22:51:00 -0300

    28/02/2024 Adiciona funções novas ao Filtro

--- lib/page/filter_page.dart
+++ lib/page/filter_page.dart
@@ -1,5 +1,6 @@
 import 'package:flutter/material.dart';
 import 'package:pmfilmes/page/genero_page.dart';
+import 'package:pmfilmes/vm/filter_item.dart';
 import 'package:pmfilmes/vm/genero_view_model.dart';
 import 'package:provider/provider.dart';
 
@@ -14,37 +15,15 @@ class FilterPage extends StatelessWidget {
         title: const Text("Filtros"),
         backgroundColor: Theme.of(context).colorScheme.inversePrimary,
       ),
-      body: ListView(
-        children: [
-          generoView(context, generoVM),
-        ],
-      ),
+      body: Wrap(children: [generoView(context, generoVM)]),
     );
   }
 
-  Widget generoView(BuildContext context, GeneroViewModel vm) => Card(
-        elevation: 5,
-        child: Padding(
-          padding: const EdgeInsets.all(4),
-          child: Row(
-            children: [
-              const Text("Genero Selecionado: "),
-              Expanded(
-                child: Text(
-                  vm.jaSelecionouGenero
-                      ? vm.generoSelecionado.nome
-                      : "Não Selecionado",
-                ),
-              ),
-              IconButton(
-                onPressed: () => Navigator.push(
-                  context,
-                  MaterialPageRoute(builder: (_) => const GeneroPage()),
-                ),
-                icon: const Icon(Icons.tune_rounded),
-              ),
-            ],
-          ),
-        ),
+  Widget generoView(BuildContext context, GeneroViewModel vm) => FilterItem(
+        title: "Gênero",
+        value:
+            vm.jaSelecionouGenero ? vm.generoSelecionado.nome : "Não informado",
+        onPressed: () => Navigator.push(
+            context, MaterialPageRoute(builder: (_) => const GeneroPage())),
       );
 }
---
+++ lib/repositorio/ator_repositorio.dart
@@ -0,0 +1,15 @@
+import 'package:pmfilmes/dominio/ator.dart';
+import 'package:pmfilmes/dominio/genero.dart';
+
+class AtorRepositorio {
+  static List<Ator> list() {
+    return <Ator>[
+      Ator(id: "aid", nome: "Jenna Ortega", nacionalidade: "Norte-Americano"),
+      Ator(id: "bid", nome: "Tyler Posey", nacionalidade: "Norte-Americano"),
+      Ator(id: "cid", nome: "Ian Somerhalder", nacionalidade: "Norte-Americano"),
+      Ator(id: "did", nome: "Tom Holland", nacionalidade: "Britânico"),
+      Ator(id: "eid", nome: "Jensen Ackles", nacionalidade: "Norte-Americano"),
+      Ator(id: "fid", nome: "Misha Collins", nacionalidade: "Norte-Americano"),
+    ];
+  }
+}
